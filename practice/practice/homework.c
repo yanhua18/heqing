173cab63e335f045c7040d21d28098696bababd3
yanhua18 <2814050659@qq.com>
2019-09-02 19:19:52 +0800

    日常练习

--- practice/practice/homework.c
+++ practice/practice/homework.c
@@ -436,6 +436,43 @@ int main()
 	return 0;
 }
 
+//9.输入一个整形数组，数组里有正数也有负数。数组中一个或连续多个整数组成一个子数组，求所有子数组的和的最大值
+int MAX_Arry(int *arr, int len)
+{
+	int max = arr[0];
+	int sum = arr[0];
+	int i = 0;
+	if (arr == NULL || len <= 1)
+	{
+		return 0;
+	}
+	for(i = 1; i < len; i++)
+	{
+		if (sum < 0)
+		{
+			sum = arr[i];
+		}
+		else
+		{
+			sum += arr[i];
+		}
+		if (sum > max)
+		{
+			max = sum;
+		}
+	}
+	return max;
+}
+
+int main()
+{
+	int arr[] = { 1, -2, 3, 10, -4, 7, 2, -5 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", MAX_Arry(arr, len));
+	system("pause");
+	return 0;
+}
+
 
 //10,在字符串中找出第一个只出现一次的字符，要求时间复杂度为O（n）
 //时间复杂度为O(n^2)
@@ -927,36 +964,12 @@ int main()
 	return 0;
 }
 
+
 #endif
 
-int MAX_Arry(int* arr, int sz)
-{
-	int MAX = arr[0];
-	int sum = arr[0];
-	int i = 1;
-	if (arr == NULL || sz <= 1)
-		return 0;
-	for (i = 1; i < sz; i++)
-	{
-		if (sum < 0)
-			sum = arr[i];
-		else
-		{
-			sum += arr[i];
-		}
-		if (sum > MAX)
-			MAX = sum;
-	}
-	return MAX;
-}
-int main()
-{
-	int arr[] = { 1, -2, 3, 10, -4, 7, 2, -5 };
-	int len = sizeof(arr) / sizeof(arr[0]);
-	printf("%d\n", MAX_Arry(arr, len));
-	system("pause");
-	return 0;
-}
+
+
+
 
 
 
