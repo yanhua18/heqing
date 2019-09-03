b6b17aefa6bdcc44c839b57f18b769187a8491e3
yanhua18 <2814050659@qq.com>
2019-09-03 21:00:31 +0800

    日常练习

--- practice/practice/homework.c
+++ practice/practice/homework.c
@@ -88,6 +88,59 @@ int main()
 	return 0;
 }
 
+//2，将字符串中的空格转换成%20
+void ReplaceBlank(char *str,int len)
+{
+	int MLen = 0;//计算字符的个数
+	int NumBlank = 0;//计算空格的数组
+	int IndexofMLen = 0;
+	int Indexofnew = 0;
+	int newlen = 0;
+	int i = 0;//从0号下标开始遍历
+	if(str == NULL || len <= 0)
+	{
+		return ;
+	}
+	while(str[i] != '\0')
+	{
+		++MLen;
+		if(str[i] == ' ')
+		{
+			++NumBlank;
+		}
+		++i;
+	}
+	newlen = MLen + NumBlank * 2;//a%20b%20c%20d a b c d
+	if(newlen > len)//newlen是扩充后数组的大小，所以一定要小于len原来大小，不然放不下
+	{
+		return;
+	}
+	IndexofMLen = MLen;
+	Indexofnew = newlen;
+	while(IndexofMLen >= 0 && Indexofnew >IndexofMLen)
+	{
+		if(str[IndexofMLen] == ' ')
+		{
+			str[Indexofnew--] = '0';
+			str[Indexofnew--] = '2';
+			str[Indexofnew--] = '%';
+		}
+		else
+		{
+			str[Indexofnew --] = str[IndexofMLen];
+		}
+		--IndexofMLen;
+	}
+}
+int main()
+{
+	char str[30] = "a b c d";
+	int len = (sizeof(str)/sizeof (str[0]));
+	ReplaceBlank(str,len);
+	printf("%s\n",str);
+	return 0;
+}
+
 //3，求第n个斐波拉契数
 int _Fibonacci(int n)
 {
