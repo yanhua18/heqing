6d97a13c101b5d973a6141d2758baafe9332ffc3
yanhua18 <2814050659@qq.com>
2019-09-04 23:09:52 +0800

    日常练习

--- practice/practice/homework.c
+++ practice/practice/homework.c
@@ -207,6 +207,35 @@ int main()
 	system("pause");
 	return 0;
 }
+//5.1
+ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+	//刚开始的时候，都指向起点，然后p1走k-1步后，p2和p1同时开始走，当p1走到最后一个的时候，p2指向的就是
+	最后一个节点
+		ListNode *p1 = pListHead;
+	ListNode *p2 = pListHead;
+	if(pListHead==NULL || k == 0) {
+		return NULL;
+	}
+	while( k-1 > 0)
+	{
+		if(p1->next != NULL)
+		{
+			p1 = p1->next;
+			--k;
+		}
+		else
+		{
+			cout<<"error"<<endl;
+			return NULL;
+		}
+	}
+	while(p1->next != NULL)
+	{
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	return p2;
+}
 
 //5.有一张单链表，编写函数求倒数第k个结点
 int Get_Locate(SListNode** pphead, int k)
